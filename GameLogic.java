b80521780b659f7f31f69fc1edc4114b4ccd2861
TheSlimeCrescent <68507989+TheSlimeCrescent@users.noreply.github.com>
2025-01-06 13:50:37 -0500

    Update GameLogic.java
    bet

--- GameLogic.java
+++ GameLogic.java
@@ -2,13 +2,23 @@ package guessWho;
 
 public class GameLogic {
 
-	public boolean gameOver() {
+	public boolean gameOver(Character character, Character chosenCharacter) {
 		
-		return false;
+		return isWinner(character, chosenCharacter);
+		
 	}
 	
-	public boolean isChoiceValid(Character[] charactersChosen, Character character) {
-		return true; 
+	
+	public boolean isChoiceValid(Character[] charactersChosen, Character chosenCharacter) {
+		
+		for (int i = 0; i < charactersChosen.length; i++) {
+			if (charactersChosen[i] == chosenCharacter) {
+				return false;
+			}
+		}
+		
+		return true;
+		
 	}
 	
 	public boolean isWinner(Character character, Character chosenCharacter) {

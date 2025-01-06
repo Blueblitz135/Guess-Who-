2d5e9d4bbbfe55892564af9da48cf333a2c618aa
Mlgawso <Mlgawso@gmail.com>
2025-01-06 13:34:50 -0500

    Add files via upload

---
+++ Characters.java
@@ -0,0 +1,83 @@
+package guessWho;
+
+public class Characters {
+
+	private String skinColor;
+	private String age;
+	private String hairColor;
+	private String eyeColor;
+	private String name;
+
+	private boolean hasGlasses;
+	private boolean hasHat;
+	private boolean hasFacialHair;
+
+	public Characters(String skinColor, String age, String hairColor, String eyeColor, String name, boolean hasGlasses, boolean hasHat, boolean hasFacialHair) {
+
+	}
+
+	public void setSkinColor(String skinColor) {
+		
+	}
+
+	public String getSkinColor() {
+
+	}
+
+	public void setAge() {
+
+	}
+
+	public String getAge() {
+
+	}
+
+	public void setHairColor() {
+
+	}
+
+	public String getHairColor() {
+
+	}
+
+	public void setEyeColor() {
+
+	}
+
+	public String getEyeColor() {
+
+	}
+
+	public void setName() {
+
+	}
+
+	public String getName() {
+
+	}
+
+	public void setHasGlasses() {
+
+	}
+
+	public boolean getHasGlasses() {
+
+	}
+
+	public void setHasHat() {
+
+	}
+
+	public boolean getHasHat() {
+
+	}
+
+	public void setHasFacialHair() {
+
+	}
+
+	public boolean getHasFacialHair() {
+
+	}
+
+}

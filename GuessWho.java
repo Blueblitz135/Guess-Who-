2c89ff07bf09a11372ee3b01f719519f082693d8
Blueblitz135 <71363016+Blueblitz135@users.noreply.github.com>
2025-01-07 15:44:26 -0500

    Update GuessWho.java
    updated the constructor and added more variables

--- GuessWho.java
+++ GuessWho.java
@@ -11,7 +11,7 @@ import javax.swing.*;
  */
 public class GuessWho extends JFrame implements ActionListener {
 	private JFrame startFrame;
-	private JFrame ruleFrame;
+	private JFrame rulesFrame;
 	private JFrame gameFrame;
 	private JLabel gameTitle;
 	private JPanel topPanel;
@@ -20,12 +20,15 @@ public class GuessWho extends JFrame implements ActionListener {
 	private JLabel nameGuessLabel;
 	private JLabel guessLabel;
 	private JLabel questiontoAsk;
+	private JButton submitQuestionButton;
+	private JButton closeRulesButton;
 	private JButton startButton;
 	private JButton rulesButton;
 	private JButton submitGuessButton;
 	private JButton rightArrowForNextQuestion;
 	private JButton leftArrowForNextQuestion;
-	private ImageIcon gameTitleImage;
+	private ImageIcon gameLogo;
+	private ArrayList<String> questionBank = new ArrayList<>();
 	
 	
 	
@@ -33,17 +36,29 @@ public class GuessWho extends JFrame implements ActionListener {
 	 * This is the constructor which will initiate the graphics of the game board
 	 */
 	public GuessWho() {
-		// Initialize the starting frame before the game starts
+		ImageIcon gameLogo = new ImageIcon("GuessWho_Icon.png");
+		
+		
+		// Initialize the starting frame before the game starts, including the start button and the title logo on the frame
 		startFrame = new JFrame("Guess Who");
 		startFrame.setSize(new Dimension(1100, 800));
-		//startFrame.setLayout();
+		startFrame.setLayout(new BoxLayout(startFrame.getContentPane(), BoxLayout.Y_AXIS));
 		startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		startFrame.getContentPane().setBackground(new Color(154, 231, 252));
+		startFrame.getContentPane().setBackground(new Color(187, 238, 252));
 		startFrame.setResizable(false);
 		startFrame.setVisible(true);
 		
+		gameTitle = new JLabel();
+		gameTitle.setIcon(gameLogo);
+		gameTitle.setVisible(true);
+		startFrame.add(gameTitle);
+		startButton = new JButton();
+		
+		
+		
 		
 		// Initialize the rules 
+		
 		
 		// Initialize objects for the game board
 		//startFrame = new JFrame();

73f1a5f818b9a4c39379f4ee007a435e483e58c6
M3rawesomeness <151587615+M3rawesomeness@users.noreply.github.com>
2025-01-10 16:45:43 -0500

    Update GuessWho.java
    Continued Aiplayer algorithm

--- GuessWho.java
+++ GuessWho.java
@@ -1,202 +1,209 @@
 package guessWho;
 
-import java.util.*;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.*;
-import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
-/**
- * This class will contain the game board and run the game Guess Who when the
- * main method calls it.
- *
- */
-public class GuessWho extends JFrame implements ActionListener {
-	private JFrame startFrame;
-	private JFrame rulesFrame;
-	private JFrame gameFrame;
-	private JLabel gameTitle;
-	private JPanel topPanel;
-	private JPanel bottomPanel;
-	private JPanel crossOutGrid;
-	private JLabel nameGuessLabel;
-	private JLabel guessLabel;
-	private JLabel questiontoAsk;
-	private JButton startButton;
-	private JButton rulesButton;
-	private JButton submitGuessButton;
-	private JButton rightArrowForNextQuestion;
-	private JButton leftArrowForNextQuestion;
-	private JButton submitQuestionButton;
-	private JButton closeRulesButton;
-	private ImageIcon gameTitleImage;
-	private ArrayList<String> questionBank;
-	private int currentQuestionIndex;
+public class AIPlayer {
 
-	/**
-	 * This is the constructor which will initiate the graphics of the game board
-	 */
-	public GuessWho() {
-		// Initialize the starting frame before the game starts
-		startFrame = new JFrame("Guess Who");
-		startFrame.setSize(new Dimension(1100, 800));
-		// startFrame.setLayout();
-		startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		startFrame.getContentPane().setBackground(new Color(154, 231, 252));
-		startFrame.setResizable(false);
-		startFrame.setVisible(true);
+	private GameChar GameChar;
+	private ArrayList<GameChar> possibleGameChars = new ArrayList<GameChar>();
+	private int questionNumber = -1;
+	private String[] attributes = { "whiteSkin", "blackSkin", "whiteHair", "brownHair", "blondeHair", "blackHair",
+			"gingerHair", "maleGender", "femaleGender", "brownEye", "blueEye", "trueGlasses", "falseGlasses", "trueHat",
+			"falseHat", "trueFaceHair", "falseFaceHair", "trueEaring", "falseEaring", "trueMustache", "falseMustache",
+			"trueTeethShowing", "falseTeethShowing" }; // All the attributes of the GameChars
 
-		// Initialize the rules
+	private int[] numberOfAttributes = new int[attributes.length]; // Number of GameChar that have the attributes,
+																	// indexes
+																	// correspond to those of the attributes
 
-		// Initialize objects for the game board
-		// startFrame = new JFrame();
-
-		/**
-		 * Creates and fills the character list
-		 */
-
-		ArrayList<Character> characters = new ArrayList<>();
-		characters.add(
-				new Character("white", "brown", "female", "brown", "RACHEL", false, true, false, false, false, false));
-
-		characters
-				.add(new Character("white", "white", "male", "brown", "JON", false, false, true, false, false, false));
-
-		characters
-				.add(new Character("white", "brown", "male", "blue", "NICK", true, false, false, false, false, false));
-
-		characters.add(
-				new Character("white", "ginger", "female", "brown", "ASHLEY", false, true, false, true, false, false));
-
-		characters
-				.add(new Character("white", "blonde", "male", "brown", "JAKE", false, false, true, false, true, false));
-
-		characters.add(
-				new Character("white", "white", "male", "brown", "JOSHUA", false, false, true, false, true, false));
-
-		characters.add(
-				new Character("white", "white", "female", "blue", "EMILY", true, false, false, true, false, false));
-
-		characters
-				.add(new Character("white", "blonde", "male", "blue", "KYLE", false, false, false, false, false, true));
-
-		characters
-				.add(new Character("white", "white", "male", "brown", "ALEX", true, false, false, false, false, false));
-
-		characters.add(
-				new Character("black", "black", "female", "brown", "SARAH", true, false, false, true, false, false));
-
-		characters.add(
-				new Character("white", "brown", "male", "brown", "JUSTIN", false, false, true, false, true, false));
-
-		characters.add(
-				new Character("white", "blonde", "female", "blue", "MEGAN", false, false, false, true, false, false));
-
-		characters.add(
-				new Character("white", "white", "male", "brown", "MATT", false, false, false, false, false, false));
-
-		characters.add(
-				new Character("white", "blonde", "male", "brown", "BRANDON", false, true, false, false, false, true));
-
-		characters.add(
-				new Character("white", "blonde", "male", "brown", "WILLIAM", false, false, true, false, false, false));
-
-		characters.add(
-				new Character("black", "brown", "male", "brown", "CONNOR", false, false, true, false, true, false));
-
-		characters.add(
-				new Character("white", "black", "male", "brown", "CHRIS", false, true, false, false, false, false));
-
-		characters.add(
-				new Character("black", "brown", "male", "brown", "DANIEL", false, true, false, false, false, false));
-
-		characters
-				.add(new Character("black", "black", "male", "brown", "ANDY", false, false, true, false, false, true));
-
-		characters.add(
-				new Character("black", "brown", "male", "brown", "JAMES", false, false, true, false, false, false));
-
-		characters.add(
-				new Character("white", "ginger", "male", "brown", "DAVID", false, false, true, false, true, false));
-
-		characters.add(
-				new Character("white", "ginger", "male", "brown", "ZACHARY", false, false, false, false, false, false));
-
-		characters.add(
-				new Character("white", "ginger", "male", "blue", "JOSEPH", true, false, false, false, false, false));
-
-		characters.add(
-				new Character("white", "black", "male", "brown", "TYLER", false, false, false, false, false, true));
-
+	public AIPlayer(ArrayList<GameChar> GameChars) {
+		possibleGameChars = GameChars;
 	}
 
 	/**
-	 * This method will create the pop up for the win screen when there is a winner.
-	 */
-	public static void resultScreen() {
-
-	}
-
-	/**
-	 * This method will call the the methods from other classes while the game is
-	 * running
+	 * Chooses a GameChar randomly from the 24 available GameChars
 	 * 
-	 * @param e the action event that was registered
+	 * @param GameChars
 	 */
-	@Override
+	public void chooseGameChar(ArrayList<GameChar> GameChars) {
+		Random rand = new Random();
+		setGameChar(GameChars.get(rand.nextInt(24)));
+	}
 
-	// actionListener has if statements to determine which question is pressed
-	//
-	public void actionPerformed(ActionEvent e) {
-		if ((e.getSource()).equals(startButton)) {
-			startFrame.setVisible(false);
-			gameFrame.setVisible(true);
-			rulesFrame.setVisible(true);
-		}
-
-		if ((e.getSource()).equals(rulesButton)) {
-			rulesFrame.setVisible(true);
-		}
-
-		if ((e.getSource()).equals(submitGuessButton)) {
-
-		}
-
-		if ((e.getSource()).equals(rightArrowForNextQuestion)) {
-			// If at the last question, loop back to the very first question
-			if (currentQuestionIndex == questionBank.size() - 1) {
-				currentQuestionIndex = 0;
-			} else {
-				currentQuestionIndex++;
-			}
-
-			questiontoAsk.setText(questionBank.get(currentQuestionIndex));
-
-		}
-
-		if ((e.getSource()).equals(leftArrowForNextQuestion)) {
-
-			// If at the first question, loop back to the very last question
-			if (currentQuestionIndex == 0) {
-				currentQuestionIndex = questionBank.size() - 1;
-			} else {
-				// Else subtract one from the index to go back to the previous index
-				currentQuestionIndex--;
-			}
-
-			questiontoAsk.setText(questionBank.get(currentQuestionIndex));
-
-		}
-
-		if ((e.getSource()).equals(closeRulesButton)) {
-			rulesFrame.setVisible(false);
-
-		}
-
-		if ((e.getSource()).equals(submitQuestionButton)) {
-
-		}
+	public void pop() {
 
 	}
 
+	/**
+	 * 
+	 * @param GameChars
+	 */
+	public GameChar playTurn(boolean answer) {
+		if (possibleGameChars.size() == 1) {
+			// Must make a guess
+			return possibleGameChars.get(0);
+		}
+
+		if (questionNumber != -1) {
+			String question = questions(questionNumber);
+			if (question.indexOf("Not") > -1) {
+				answer = !answer;
+			}
+			for (GameChar i : possibleGameChars) {
+				if (question.indexOf("Hair") > -1) {
+					if (question.indexOf("White") > -1) {
+						if (i.getHairColor().equals("white") && answer == true) {
+							continue;
+						} else {
+							possibleGameChars.remove(i);
+						}
+					} else if (question.indexOf("Brown") > -1) {
+						if (i.getHairColor().equals("brown") && answer == true) {
+							continue;
+						} else {
+							possibleGameChars.remove(i);
+						}
+					} else if (question.indexOf("Blonde") > -1) {
+						if (i.getHairColor().equals("blonde") && answer == true) {
+							continue;
+						} else {
+							possibleGameChars.remove(i);
+						}
+					} else if (question.indexOf("Ginger") > -1) {
+						if (i.getHairColor().equals("ginger") && answer == true) {
+							continue;
+						} else {
+							possibleGameChars.remove(i);
+						}
+					}
+				} else if (question.indexOf("Eye") > -1) {
+					if (question.indexOf("Blue") > -1) {
+						if (i.getEyeColor().equals("blue") && answer == true) {
+							continue;
+						} else {
+							possibleGameChars.remove(i);
+						}
+					} else if (question.indexOf("Brown") > -1) {
+						if (i.getEyeColor().equals("brown") && answer == true) {
+							continue;
+						} else {
+							possibleGameChars.remove(i);
+						}
+					}
+				} else if (question.indexOf("Skin") > -1) {
+					if (question.indexOf("white") > -1) {
+						if (i.getSkinColor().equals("brown") && answer == true) {
+							
+						}
+					}
+				}
+			}
+		}
+
+		for (
+
+				int i = 0; i < possibleGameChars.size(); i++) {
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getSkinColor())]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getHairColor())]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getGender() + "Gender")]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getEyeColor() + "Eye")]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getHasGlasses() + "Glasses")]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getHasHat() + "Hat")]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getHasFacialHair() + "FaceHair")]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getHasEarings() + "Earing")]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getHasMustache() + "Mustache")]++;
+			numberOfAttributes[getIndex(possibleGameChars.get(i).getIsShowingTeeth() + "TeethShowing")]++;
+		}
+
+		int highest = -1;
+		for (int i = 0; i < numberOfAttributes.length; i++) {
+			if (numberOfAttributes[i] > highest) {
+				questionNumber = i;
+				highest = numberOfAttributes[i];
+			}
+		}
+
+		questions(questionNumber);
+		return null;
+	}
+
+	public int getIndex(String key) {
+		for (int i = 0; i < attributes.length; i++) {
+			if (attributes[i] == key) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * This method should be called in the panel which holds the question which the
+	 * ai is asking
+	 * 
+	 * @param questionNumber
+	 * @return
+	 */
+
+	public String questions(int questionNumber) {
+		switch (questionNumber) {
+
+		case 0:
+			return "Does Your Character Have White Skin?";
+		case 1:
+			return "Does Your Character Have Black Skin?";
+		case 2:
+			return "Does Your Character Have White Hair?";
+		case 3:
+			return "Does Your Character Have Brown Hair?";
+		case 4:
+			return "Does Your Character Have Blonde Hair?";
+		case 5:
+			return "Does Your Character Have Ginger Hair?";
+		case 6:
+			return "Is Your Character Male?";
+		case 7:
+			return "Is Your Character Female?";
+		case 8:
+			return "Does Your Character Have Brown Eyes?";
+		case 9:
+			return "Does Your Character Have Blue Eyes?";
+		case 10:
+			return "Does Your Character Have Glasses";
+		case 11:
+			return "Does Your Character Not Have Glasses?";
+		case 12:
+			return "Is Your Character Wearing A Hat?";
+		case 13:
+			return "Is Your Character Not Wearing A Hat?";
+		case 14:
+			return "Does Your Character Have Facial Hair";
+		case 15:
+			return "Does Your Character Not Have Facial Hair";
+		case 16:
+			return "Does Your Character Have Earings";
+		case 17:
+			return "Does Your Character Not Have Earings";
+		case 18:
+			return "Does Your Character Have A Mustache";
+		case 19:
+			return "Does Your Character Not Have A Mustache";
+		case 20:
+			return "Is Your Character Showing Teeth?";
+		case 21:
+			return "Is Your Character Not Showing Teeth?";
+		}
+		return "Error";
+	}
+
+	public GameChar getGameChar() {
+		return this.GameChar;
+	}
+
+	public void setGameChar(GameChar GameChar) {
+		this.GameChar = GameChar;
+	}
 }
